2f31c2a631855baf41406aae6b8c911dee64f8ea
chupapiponhanho2004-pixel <chupapiponhanho2004@gmail.com>
2026-04-15 01:32:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,13 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: '/AI/',
+    
+
+base: '/thochobe/',     
+server: {
+  port: 3000,
+  host: '0.0.0.0',
+},
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
